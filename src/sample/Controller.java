a10e00d69bcce325ce734da9fc315ddf1264fc49
jamesmqle <jamesmqle01@gmail.com>
2021-02-06 01:10:33 -0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Controller {
+
+    @FXML
+    private Button buttonEnergetic;
+
+    @FXML
+    private Button buttonNeutral;
+
+    @FXML
+    private Button buttonSore;
+
+    @FXML
+    public void ButtonOnAction(ActionEvent event){
+
+    }
+}
