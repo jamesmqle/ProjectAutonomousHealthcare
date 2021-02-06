e43f7f65f07d79041396a75a007a0ae25f6bb0e2
jamesmqle <jamesmqle01@gmail.com>
2021-02-06 02:03:16 -0500

    updates - jamesmqle

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,22 +1,42 @@
 package sample;
 
 import javafx.fxml.FXML;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
 public class Controller {
     
-    public void buttonEnergetic(ActionEvent event){
+    
+    @FXML
+    public void handleButtonEnergetic(ActionEvent event){
         System.out.println("GET HYPEEDD!!!");
     }
 
-    public void buttonSore(ActionEvent event){
+    public void handleButtonSore(ActionEvent event){
         System.out.println("Too bad so sad!!!");
     }
-    public void buttonNeutral(ActionEvent event){
+    public void handleButtonNeutral(ActionEvent event){
         System.out.println("bleh");
+    }
+    
+    /*
+    When this method is called, it will change the Scene to exercise view 
+     */
+    @FXML
+    public void changeScreenButtonPushed(ActionEvent event) throws IOException {
+        Parent exerciseParent = FXMLLoader.load(getClass().getResource("exercise.fxml"));
+        Scene exerciseScene = new Scene(exerciseParent);
+
+        //This line gets the Stage Information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(exerciseScene);
+        window.show();
     }
     
 }
