88cd489ae2f4037e703c678546cd52d6e5344990
Stanley Ihesiulo <stanihe1901@gmail.com>
2021-02-06 14:32:58 -0500

    stuff

--- src/sample/DataBase.java
+++ src/sample/DataBase.java
@@ -1,4 +1,27 @@
 package sample;
 
+import GUI.Excercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class DataBase {
+    public static List<String> excerciseTypes;
+    public static List<Excercise> excerciseSessions;
+    public static int excludeExcerciseCount = 3;
+
+    public static String getExcerciseRecommendation() {
+        Random rand = new Random();
+        List<String> tmpExcercistTypes = new ArrayList<>().addAll(excerciseTypes);
+        for (int i=excerciseSessions.size()-1-excludeExcerciseCount; i<excerciseSessions.size(); i++)
+
+
+        rand.nextInt(excerciseTypes.size());
+    }
+
+    public static void addSession(Excercise excercise) {
+        if (excercise.hasEnded)
+            excerciseSessions.add(excercise);
+    }
 }
